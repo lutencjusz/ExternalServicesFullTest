97371cd4b5893552889ce94ed3125afe2fead4c4
lutencjusz <lutencjusz@gmail.com>
2024-08-20 22:52:45 +0200

    Spring cache

--- src/main/java/com/example/ExternalApi/ExternalApiRepository.java
+++ src/main/java/com/example/ExternalApi/ExternalApiRepository.java
@@ -14,19 +14,21 @@ public class ExternalApiRepository {
 
     Dotenv dotenv = Dotenv.load();
 
-    private static final String URL = "https://api.openweathermap.org/data/2.5/";
+    private static final String URI = "https://api.openweathermap.org/data/2.5/";
+    public static final String WEATHER_GET_EXTERNAL_DATA_URI = "weather?lat={lat}&lon={lom}&appid={apiKey}&lang=pl&units=metric";
+
     String API_KEY = dotenv.get("API_KEY");
 
     RestTemplate restTemplate = new RestTemplate();
 
     private <T> T getGetDataFromExternalData(String url, Class<T> responseType, Object... objects) {
-        return restTemplate.getForObject(URL + url, responseType, objects);
+        return restTemplate.getForObject(URI + url, responseType, objects);
 
     }
 
     @Cacheable("externalData")
     public WeatherDto getExternalData(double lat, double lon) {
-        OpenWeatherDto openWeatherDto = getGetDataFromExternalData("weather?lat={lat}&lon={lom}&appid={apiKey}&lang=pl&units=metric", OpenWeatherDto.class, lat, lon, API_KEY);
+        OpenWeatherDto openWeatherDto = getGetDataFromExternalData(WEATHER_GET_EXTERNAL_DATA_URI, OpenWeatherDto.class, lat, lon, API_KEY);
         return WeatherDto.builder()
                 .temperature((float) openWeatherDto.getMain().getTemp())
                 .feelsLike((float) openWeatherDto.getMain().getFeels_like())
